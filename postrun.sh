73761c7502ef78571d22d79142b64f60cdd1e139
sanjarbek <sanjarsaidov173@gmail.com>
2023-01-28 14:20:11 +0500

    new libraries

--- postrun.sh
+++ postrun.sh
@@ -1,2 +1,7 @@
 # docker run --name my-postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e PGDATA='/var/run/postgresql/.s.PGSQL.5432' -d postgres
-docker run --name my-postgres -e PGDATA=/var/run/postgresql -ePOSTGRES_DB=mydatabase -e POSTGRES_USER=django -e POSTGRES_PASSWORD=admin -d postgis/postgis
+docker run --name my-postgres \
+-e PGDATA=/var/run/postgresql \
+-e POSTGRES_DB=mydatabase \
+-e POSTGRES_USER=django \
+-e POSTGRES_PASSWORD=admin \
+-d postgis/postgis
--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-docker run --name myimage --link my-postgres:postgres -v='C:\Users\User\Documents\backend\geoDjango-tutorial':/home -p 8000:8000 -it myimage
+docker run --name myimage \
+--link my-postgres:postgres \
+-v='C:\Users\User\Documents\backend\geoDjango-tutorial':/home \
+-p 8000:8000 -it myimage
